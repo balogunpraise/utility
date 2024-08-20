a90445ce04dfd96675fa65b0f9c4b0bc31a9886f
balogunpraise <balogunpraise2@gmail.com>
2024-08-20 21:14:38 +0100

    modified

---
+++ UtilityApp/Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UtilityApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
